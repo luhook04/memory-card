f5bede05a75047c290b70673adac4430905413f1
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-03-18 17:57:26 -0500

    Implement shuffleArray function using the Fisher-Yates Shuffle method

--- src/App.js
+++ src/App.js
@@ -27,7 +27,22 @@ const App = () => {
 
   const handleClick = () => {};
 
-  const shuffleArray = () => {};
+  const shuffleArray = (array) => {
+    let currentIndex = array.length,
+      randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [ array[currentIndex], array[randomIndex] ] = [
+        array[randomIndex],
+        array[currentIndex]
+      ];
+    }
+
+    return array;
+  };
 };
 
 export default App;
