22e823e5675ef2ce6e4f49e1a053e91f370b0107
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-03-28 19:39:00 -0400

    Set up static version of Scoreboard component

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scoreboard = () => {
   return (
-    <div>
+    <div className="scoreboard">
       <p>Current Score: {currentScore}</p>
       <p>Best Score: {bestScore}</p>
     </div>
