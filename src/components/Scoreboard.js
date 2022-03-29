ada7525d3ac39a10e815b145118ad05259c0b76b
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-03-29 16:09:39 -0400

    Add more style changes

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const Scoreboard = ({ currentScore, bestScore }) => {
   return (
-    <div className="scoreboard">
-      <p>Current Score: {currentScore}</p>
-      <p>Best Score: {bestScore}</p>
+    <div>
+      <h1>Pokemon Memory Game</h1>
+      <div className="scoreboard">
+        <p>Current Score: {currentScore}</p>
+        <p>Best Score: {bestScore}</p>
+      </div>
     </div>
   );
 };
